b47c08a2e436fc497c9528e8fe191c0b5bdd6c6b
semanticbits-sandeep <sandeep.cherukuri93@gmail.com>
2017-05-13 16:18:05 +0530

    TrainingProject

---
+++ src/app/newsfeed/newsfeed.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
+import {Http} from '@angular/http';
+import {HTTPTestService} from "./http-test.service"
+import {error} from "util";
+import {computeMsgId} from "@angular/compiler/src/i18n/digest";
+import {until} from "selenium-webdriver";
+import titleContains = until.titleContains;
+
+//import { ConfirmComponent } from '.confirm/confirm.component';
+@Component({
+  selector: 'app',
+  templateUrl: './newsfeed.component.html',
+  styleUrls: ['./newsfeed.component.css'],
+  providers:[HTTPTestService]
+})
+export class NewsfeedComponent {
+  public data;
+  public news;
+  public com;
+  public com_title;
+  public comment1;
+
+  constructor(public _httpService:HTTPTestService) {
+    this._httpService.getjsondata()
+      .subscribe(data => this.data = data,
+        error=>alert(error),
+        ()=>console.log("Finished")
+
+      );
+    this._httpService.getcommdata()
+      .subscribe(com=>this.com=com,
+      error=>alert(error),
+        ()=>console.log("Finished2"));
+    //console.log(this.com);
+  }
+  ontheData(){
+    console.log(this.data);
+    alert(this.data);
+
+  }
+  add(){
+    var tit=(<HTMLInputElement>document.getElementById("tit")).value;
+    var post=(<HTMLInputElement>document.getElementById("post")).value;
+    var obj={
+      title: tit,
+        post:post
+    };
+    var obj1={
+      title:tit,
+        comment:[]
+    }
+    this.com.push(obj1);
+    this.data.push(obj);
+    this.clear();
+  }
+  dis(name){
+    this.comment1=[];
+    this.com_title=name;
+    //alert(name);
+    for(let co=0;co<this.com.length;co++){
+      //console.log(this.com[co].comment);
+      if(this.com[co].title===name){
+        for(let j=0;j<this.com[co].comment.length;j++){
+          this.comment1.push(this.com[co].comment[j])
+         // console.log(this.comment1);
+        }
+
+      }
+    }
+
+  }
+  clear(){
+    (<HTMLInputElement>document.getElementById("tit")).value="";
+    (<HTMLInputElement>document.getElementById("post")).value="";
+    (<HTMLInputElement>document.getElementById("com_com")).value="";
+  }
+  add_com(dc){
+    for(let co=0;co<this.com.length;co++){
+      if(this.com[co].title===dc){
+        let new_comment=(<HTMLInputElement>document.getElementById("com_com")).value
+        this.com[co].comment.push(new_comment);
+      }
+    }
+    this.clear();
+
+  }
+}
